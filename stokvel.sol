34ad019585fd1898a978010fed9e8433fc7dd21a
lemogang-ai <ntlhailel@gmail.com>
2026-04-06 11:27:26 +0200

    add state variables and constructor

--- stokvel.sol
+++ stokvel.sol
@@ -3,5 +3,17 @@
 pragma solidity 0.8.18; // this is the solidity version , 
 
 contract savingCircle {
+    uint256 public constant Contribution_amount = 0.001 ether;
+    uint256 public maxMembers; // members in the saving circle
+    uint256 public payOutAmount; // total payout per cycle 
+    address[] public members; 
+
+    constructor(uint256 _maxMembers){
+        maxMembers = _maxMembers;
+        payOutAmount = Contribution_amount * _maxMembers;
+    }
+
+
+
 
 }   
